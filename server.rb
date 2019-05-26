51196bc50ac3c49eb096465b2eb9c5b4760ee072
Delton Ding <dsh0416@gmail.com>
2019-05-27 01:10:35 +0900

    Minor updates

--- server.rb
+++ server.rb
@@ -6,5 +6,6 @@ Dir[File.dirname(__FILE__) + '/routes/*.rb'].each { |file| require file }
 require './handlers/runtime_error'
 Dir[File.dirname(__FILE__) + '/handlers/*.rb'].each { |file| require file }
 Dir[File.dirname(__FILE__) + '/initializers/*.rb'].each { |file| require file }
+require './services/experiment'
 Dir[File.dirname(__FILE__) + '/services/*.rb'].each { |file| require file }
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each { |file| require file }
